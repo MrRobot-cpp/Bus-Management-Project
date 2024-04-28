4dd3b8be1e0d8cf5a074dfbff9de56546fded30d
Ahmed Ghazouly <medoghazouly10@gmail.com>
2024-04-29 00:23:53 +0300

    studentView added

---
+++ Bus-Management-Project/client/src/StudentView/src/StudentProfile/StudentProfile.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./StudentProfile.css";
+
+
+function StudentProfile() {
+
+
+    return(
+        <>
+        <div className="card-container">
+         <div className="card">
+                <div className="card-img">
+                </div>
+                <div className="card-info">
+                    <div className="card-logo"><i class="fa-regular fa-user icon"></i> </div>
+                    <h2 className="card-cat">Profile</h2>
+                    <strong className="card-title">                
+                     </strong>
+                </div>
+            </div>
+        </div>
+        </>
+        );
+}
+export default StudentProfile;
