5008d84d1e42086818f5bf2110217da599cc7b14
Ahmed Ghazouly <medoghazouly10@gmail.com>
2024-04-29 01:19:55 +0300

    Import was deleted

--- Bus-Management-Project/client/src/StudentView/src/App.jsx
+++ Bus-Management-Project/client/src/StudentView/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   return(
     <>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
     <div className='main-container'>
      <VerticalNavbar/>
      <StudentProfile/>
