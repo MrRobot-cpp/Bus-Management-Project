fdb4af83accfe0d037f7bad70ac86ad6d4ff109c
angie elsegeiny <elsegeinyangie@gmail.com>
2024-04-29 20:05:31 +0300

    header created

--- Bus-Management-Project/client/src/LandingPage/src/App.jsx
+++ Bus-Management-Project/client/src/LandingPage/src/App.jsx
@@ -1,12 +1,58 @@
-// import MenuItemButtonRegular from "./Componets/MenuItemButton/MenuItemButtonRegular.jsx"
+import MenuItemButtonRegular from "./Componets/MenuItemButton/MenuItemButtonRegular.jsx"
 import MenuItemButtonHighlighted from "./Componets/MenuItemButton/MenuItemButtonHighLighted.jsx";
+import { useState } from 'react';
+
+
 
 
 function App() {
 
+  const [toggleOpen, setToggleOpen] = useState(false);
+
+  const toggleDropDown = () => {
+    setToggleOpen(!toggleOpen);
+
+    const toggleBtnIcon = document.querySelector('.toggle-btn-logo');
+    const dropDownMenu = document.querySelector('.dropdown-menu');
+
+    dropDownMenu.classList.toggle('open');
+    const isOpen = dropDownMenu.classList.contains('open');
+    //check if class "open" is in dropdown menu element (if present, value true)
+    toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
+  };
+
+
   return (
-    <> 
-       <MenuItemButtonHighlighted/>
+    <>
+
+    <nav className="header">
+      <div className="header-contents">
+          <h1 className="logo-placeholder">RouteMinder</h1>
+          <ul className="unordered-list">
+              <li className="list"><MenuItemButtonRegular text="Home" href="#" /></li>
+              <li className="list"><MenuItemButtonRegular text="About" href="#" /></li>
+              <li className="list"><MenuItemButtonRegular text="Services" href="#" /></li>
+              <li className="list"><MenuItemButtonRegular text="Contact Us" href="#" /></li>
+          </ul>
+
+          <MenuItemButtonHighlighted text="Login" />
+          <div className="toggle-btn" onClick={toggleDropDown}>
+              <i className="fa-solid fa-bars toggle-btn-logo"></i>
+          </div>
+
+          <div className="dropdown-menu" >
+              <li className="list"><MenuItemButtonRegular text="Home" href="#" /></li>
+              <li className="list"><MenuItemButtonRegular text="About" href="#" /></li>
+              <li className="list"><MenuItemButtonRegular text="Services" href="#" /></li>
+              <li className="list"><MenuItemButtonRegular text="Contact Us" href="#" /></li>
+              <li className="list"> <MenuItemButtonHighlighted text="Login" className="login-btn" /></li>
+          </div>
+
+      </div>
+    </nav>
+
+
+  
     </>
   );
 }
