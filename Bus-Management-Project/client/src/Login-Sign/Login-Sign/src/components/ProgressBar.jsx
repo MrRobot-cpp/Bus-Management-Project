3086b7e5107db950b867487c40b739a2bc5c12ac
Shady-Yasser <shadyyasset@gmail.com>
2024-05-01 01:28:11 +0300

    map and length error in progress bar was fixed

--- Bus-Management-Project/client/src/Login-Sign/Login-Sign/src/components/ProgressBar.jsx
+++ Bus-Management-Project/client/src/Login-Sign/Login-Sign/src/components/ProgressBar.jsx
@@ -1,6 +1,7 @@
-import "./ProgressBar.css"
+/* eslint-disable react/prop-types */
+import "./ProgressBar.css";
 
-function ProgressBar(props){
+function ProgressBar(props) {
     // eslint-disable-next-line react/prop-types
     const { departure: { hour: depHour, minute: depMinute, period: depPeriod } 
     = { hour: 0, minute: 0, period: 'AM' },
@@ -8,25 +9,21 @@ function ProgressBar(props){
     arrival: { hour: arrHour, minute: arrMinute, period: arrPeriod } 
     = { hour: 0, minute: 0, period: 'AM' },
     // eslint-disable-next-line react/prop-types
-    stops} = props;
-    return(
+    stops } = props;
+    return (
         <>
-        <div className="progress-bar">
-        {stops.map((stop,index)=>(<>
-        <span className="progress-bar-time-line">
-        {index === 0 && <p className="progress-bar-time-line-text">{`${depHour}:${depMinute} ${depPeriod}`}</p>}
-        </span>
-        <>
-        <div className="circle">{stop}</div>
-        {index !== stops.length - 1 && <div className="bar"></div>}
-        <span className="progress-bar-time-line">
-        {index === stops.length - 1 && <p className="progress-bar-time-line-text">{`${arrHour}:${arrMinute} ${arrPeriod}`}</p>}
-        </span>
-        </>
-        </>
-    ))}
-        </div>
+            <div className="progress-bar">
+                {stops.map((stop, index) => (
+                    <div key={index} className="progress-bar-time-line">
+                        {index === 0 && <p className="progress-bar-time-line-text">{`${depHour}:${depMinute} ${depPeriod}`}</p>}
+                        <div className="circle">{stop}</div>
+                        {index !== stops.length - 1 && <div className="bar"></div>}
+                        {index === stops.length - 1 && <p className="progress-bar-time-line-text">{`${arrHour}:${arrMinute} ${arrPeriod}`}</p>}
+                    </div>
+                ))}
+            </div>
         </>
     );
 }
-export default ProgressBar
+
+export default ProgressBar;
