916be7d0523fa93d65da793c303750c3e5c109ef
Shady-Yasser <shadyyasset@gmail.com>
2024-05-01 00:47:30 +0300

    modified the file management moved from the student view to login-signup

--- Bus-Management-Project/client/src/Login-Sign/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/Login-Sign/src/App.jsx
@@ -1,14 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import Login from './components/Login.jsx';
+//import Login from './components/Login.jsx';
 //import Signup from './components/signUp.jsx';
-
+import StudentProfile from './components/StudentProfile';
 function App() {
     return (
-        <div>
-            <Login />
-            {/* <Signup /> */}
-        </div>
+        <>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <div className='main-container'>
+    
+    <StudentProfile/>
+    </div>
+</>
     );
 }
 
