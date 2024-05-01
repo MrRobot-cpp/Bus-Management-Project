b8a51ece2e3cc3757c9aab756652c3a19f783983
Shady-Yasser <shadyyasset@gmail.com>
2024-05-01 23:48:35 +0300

    ignore

--- Bus-Management-Project/client/src/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/App.jsx
@@ -1,25 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-// import Login from './components/Login.jsx';
+//import Login from './components/Login.jsx';
 //import Signup from './components/signUp.jsx';
 //import StudentProfile from './components/StudentProfile/StudentProfile/';
 //import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
-import Student from './components/Student/Student';
+//import Student from './components/Student/Student';
 //import Home from './components/Home'
 function App() {
     return (
         <>
             {/* <Login /> */}
             {/* <Signup /> */}
-<<<<<<< HEAD
+
             {/* <VerticalNavbar /> */}
             {/* <Home/> */}
-=======
+
             {/* <VerticalNavbar />
             <Home/>  */}
->>>>>>> c01d7f26c32601ff5dc2417b3dbf83486b68d3a3
+
             {/* <StudentProfile />  */}
-            <Student/>  
+             {/* <Student/>   */}
         </>
     );
 }
