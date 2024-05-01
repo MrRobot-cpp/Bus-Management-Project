09e052e6c5b4daf3fba88d52a50676cf8f6e6535
Ahmed-Elghazouly <medoghazouly10@gmail.com>
2024-05-01 17:42:29 +0300

    App.jsx modification

--- Bus-Management-Project/client/src/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/App.jsx
@@ -2,30 +2,25 @@
 import React from 'react';
 import Login from './components/Login.jsx';
 //import Signup from './components/signUp.jsx';
-<<<<<<< HEAD
 // import StudentProfile from './components/StudentProfile/StudentProfile/';
 // import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
- import Student from './components/Student/Student';
+import Student from './components/Student/Student';
 // import Home from './components/Home'
-=======
 //import StudentProfile from './components/StudentProfile/StudentProfile/';
 //import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
 //import Student from './components/Student/Student';
 //import Home from './components/Home'
->>>>>>> d09339e74f8965ab794623f48d3be3821f8664c9
 function App() {
     return (
         <>
             <Login />
             {/* <Signup /> */}
-<<<<<<< HEAD
 
             {/* <VerticalNavbar /> */}
             {/* <Home/> */}
-=======
+
             {/* <VerticalNavbar />
             <Home/>  */}
->>>>>>> d09339e74f8965ab794623f48d3be3821f8664c9
             {/* <StudentProfile />  */}
             <Student/> 
         </>
