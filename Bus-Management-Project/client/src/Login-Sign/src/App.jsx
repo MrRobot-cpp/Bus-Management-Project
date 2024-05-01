e1a6720cbb68cb934c0640a637afea4e49101952
Ahmed-Elghazouly <medoghazouly10@gmail.com>
2024-05-01 06:23:08 +0300

    imports management

--- Bus-Management-Project/client/src/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/App.jsx
@@ -2,13 +2,8 @@
 import React from 'react';
 // import Login from './components/Login.jsx';
 //import Signup from './components/signUp.jsx';
-<<<<<<< HEAD
 import StudentProfile from './components/StudentProfile/StudentProfile/';
-import VerticalNavbar from './components/VerticalNavbar';
-=======
-import StudentProfile from './components/StudentProfile';
 import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
->>>>>>> 1d597cfa8d72bc0f57fb4a6a550c6ffe5fb18b78
 function App() {
     return (
         <div>
