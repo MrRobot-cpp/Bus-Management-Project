e01742c5ed44e2d69a312e9eb2a1f5a946241c83
Ahmed Samer <ahmedsamersayed22@gmail.com>
2024-05-01 04:18:36 +0300

    ignore

--- Bus-Management-Project/client/src/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/App.jsx
@@ -2,27 +2,15 @@
 import React from 'react';
 // import Login from './components/Login.jsx';
 //import Signup from './components/signUp.jsx';
-<<<<<<< HEAD:Bus-Management-Project/client/src/Login-Sign/Login-Sign/src/App.jsx
 import StudentProfile from './components/StudentProfile';
 import VerticalNavbar from './components/VerticalNavbar';
-=======
-//import StudentProfile from './components/StudentProfile';
-import VerticalNavbar from './components/VerticalNavbar';
-import Home from './components/Home'
->>>>>>> de276c8420ae4059264cae2b0d25c3acd78be926:Bus-Management-Project/client/src/Login-Sign/src/App.jsx
 function App() {
     return (
         <div>
             {/* <Login /> */}
             {/* <Signup /> */}
-<<<<<<< HEAD:Bus-Management-Project/client/src/Login-Sign/Login-Sign/src/App.jsx
             <VerticalNavbar />
             <StudentProfile />
-=======
-            <Home />
-            <VerticalNavbar />
-            {/* <StudentProfile /> */}
->>>>>>> de276c8420ae4059264cae2b0d25c3acd78be926:Bus-Management-Project/client/src/Login-Sign/src/App.jsx
         </div>
     );
 }
