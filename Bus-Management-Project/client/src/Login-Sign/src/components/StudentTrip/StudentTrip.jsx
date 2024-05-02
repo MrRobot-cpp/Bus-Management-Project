9ef1468881e81959db935196a5996867a805116c
Ahmed-Elghazouly <medoghazouly10@gmail.com>
2024-05-02 05:15:38 +0300

    outline for sudent trips

--- Bus-Management-Project/client/src/Login-Sign/src/components/StudentTrip/StudentTrip.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/components/StudentTrip/StudentTrip.jsx
@@ -5,9 +5,16 @@ function StudentTrip() {
 
     return(
         <>
-        
-        
-        
+        <div className='student-trip-container'>
+        <div className='student-trip'>
+        <div className='head'>
+            <h1 className='head-txt'>Trips</h1>
+        </div>
+        <div className='trip-content'>
+        <p> random</p>
+        </div>
+        </div>
+        </div>
         
         </>
     );
