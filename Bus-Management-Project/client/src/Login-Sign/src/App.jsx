ac5fe3ef7b9139c0a6c4b33c04192b80ed113eab
Shady-Yasser <shadyyasset@gmail.com>
2024-05-03 01:09:15 +0300

    file management, ignore

--- Bus-Management-Project/client/src/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/App.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 //import StudentProfile from './components/StudentProfile/StudentProfile/';
 //import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
 // import Student from './components/Student/Student';
-import Driver from './components/Driver/Driver';
+//import Driver from './components/Driver/Driver';
 //import Home from './components/Home'
 // import AccountConfig from './components/AccountConfig';
 //import Admin from './components/Admin/Admin'
