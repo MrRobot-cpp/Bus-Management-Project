bd169b18e71f2fb2fd5ac537f4e6788fc51e7744
Ahmed Ghazouly <medoghazouly10@gmail.com>
2024-05-03 01:29:57 +0300

    ignore

--- Bus-Management-Project/client/src/Login-Sign/src/App.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/App.jsx
@@ -1,11 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-//import Login from './components/Login.jsx';
-//import Signup from './components/signUp.jsx';
-//import StudentProfile from './components/StudentProfile/StudentProfile/';
-//import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
+// import Login from './components/Login.jsx';
+// import Signup from './components/signUp.jsx';
+// import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
 import Student from './components/Student/Student';
-//import Home from './components/Home'
+// import Home from './components/Home'
 // import AccountConfig from './components/AccountConfig';
 // import Admin from './components/Admin/Admin'
 function App() {
