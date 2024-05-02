f3bdbdaf7c0128ea42db48456bb2f20ec013261c
Ahmed Samer <ahmedsamersayed22@gmail.com>
2024-05-03 00:01:23 +0300

    finished the data orgnizer made its overlay , edited all the home components styling

--- Bus-Management-Project/client/src/Login-Sign/src/components/DateOrganizer.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/components/DateOrganizer.jsx
@@ -1,4 +1,6 @@
 import styles from "./DataOrganizer.module.css";
+import Overlay from "./Overlay/Overlay";
+import React, {useState} from "react";
 
 const currentDate = new Date();
 
@@ -25,64 +27,99 @@ const currentMonth = monthsOfYear[month];
 
 const currentDayOfMonth = currentDate.getDate();
 
+
 function DateOrganizer(props) {
   const { trips } = props;
+  const [toggleStates, setToggleStates] = useState(new Array(trips.length).fill(false));
 
-  let tripTimeValidation = function(trip){
-    if(trip.date.getDate() === currentDayOfMonth && trip.date.getMonth === currentMonth){
-        return true;
-    } else{return false;}
+  function handleTripClick(index) {
+  const newToggleStates = [...toggleStates];
+  newToggleStates[index] = !newToggleStates[index];
+  setToggleStates(newToggleStates);
+}
 
-  }
 
-  console.log(trips);
+  
+
   return (
     <>
       <link src="https://kit.fontawesome.com/a076d05399.js" />
       <div className={styles["data-organizer"]}>
         <div className={styles["data"]}>
-          <h2>Schedule</h2>
+          <h2 className={styles["data-organizer-header"]}>Schedule</h2>
 
-          <span>
+          <div className={styles["current-date-container"]}>
             <p>
               {currentDayOfMonth} {currentMonth},{currentDay}
             </p>
-            <p>
-              <i className="fas fa-angle-down"></i>
-            </p>
-          </span>
+          </div>
 
           <div className={styles["current-data"]}>
-            <ul className="trip-list">
+            <ul className={styles["trip-list"]}>
               {trips.map((trip, index) => {
-               
-                  tripTimeValidation(trip) && (
-                    <li key={index}>
+                if (
+                  trip.date.getDate() === currentDayOfMonth &&
+                  trip.date.getMonth() === month
+                ) {
+                  return (
+                    <li key={index} onClick={() => handleTripClick(index)}>
+                    {toggleStates[index] && <Overlay trip={trip} />}
                       <div className={styles["trip-info"]}>
-                        <h2>Trip {index + 1}</h2>
+                        <h2 className={styles["trip-header"]}>
+                          Trip {index + 1}
+                        </h2>
                         <p>
                           {trip.departure.hour}:{trip.departure.minute}{" "}
                           {trip.departure.period} - {trip.arrival.hour}:
-                          {trip.arrival.minute} {trip.arrival.period}
+                          {trip.arrival.minute} {trip.arrival.period}{" "}
+                          <br/>Top Speed: {trip.speedLimit} mph
                         </p>
-                        <p>Speed: {trip.speedLimit} MPH</p>
+                        <br />
                         <p>Dep. Station: {trip.startPoint}</p>
                         <p>Arr. Station: {trip.endPoint}</p>
                       </div>
                     </li>
-                  )
+                  );
+                }
+                return null; // Return null if the condition is not met
               })}
             </ul>
           </div>
 
-          <span>
-            <h3>Upcoming</h3>
-            <h3>
-              <i className="fas fa-angle-down"></i>
-            </h3>
-          </span>
+             <p>Upcoming</p>
 
-          <div className={styles["upcoming-data"]}></div>
+
+          <div className={styles["upcoming-data"]}>
+          <ul className={styles["trip-list"]}>
+              {trips.map((trip, index) => {
+                if (
+                  !(trip.date.getDate() === currentDayOfMonth &&
+                  trip.date.getMonth() === month)
+                ) {
+                  return (
+                    <li key={index} onClick={() => handleTripClick(index)}>
+                    {toggleStates[index] && <Overlay trip={trip} />}
+                      <div className={styles["trip-info"]}>
+                        <h2 className={styles["trip-header"]}>
+                          Trip {index + 1}
+                        </h2>
+                        <p>
+                          {trip.departure.hour}:{trip.departure.minute}{" "}
+                          {trip.departure.period} - {trip.arrival.hour}:
+                          {trip.arrival.minute} {trip.arrival.period}{" "}
+                        <br/>Top Speed: {trip.speedLimit} mph
+                        </p>
+                        <br />
+                        <p>Dep. Station: {trip.startPoint}</p>
+                        <p>Arr. Station: {trip.endPoint}</p>
+                      </div>
+                    </li>
+                  );
+                }
+                return null; // Return null if the condition is not met
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </>
