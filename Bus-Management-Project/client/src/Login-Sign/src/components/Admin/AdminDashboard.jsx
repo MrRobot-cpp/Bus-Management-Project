0212779340917695500ebc6620ca26ab4e4012d7
rahmawael0 <rahma2201758miuegypt.edu.eg>
2024-05-03 16:01:58 +0300

    header of Students and img was done

--- Bus-Management-Project/client/src/Login-Sign/src/components/Admin/AdminDashboard.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/components/Admin/AdminDashboard.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import './AdminDashboard.css'
 import PageHeader from '../Student/PageHeader/PageHeader'
 import VerticalNavbar from '../General/VerticalNavbar'
+import studentImage from './User.png'; // Import the image
+
 function AdminDashboard() {
   const menuItems = [
     {text: "Routes" },
@@ -15,10 +17,19 @@ function AdminDashboard() {
   return (
     <div className='Dashboard-Container'>
     <PageHeader text='Admin Dashboard' />
+    <div className='main-container'>
     <div className='tempNav'>
     <VerticalNavbar menuItems={menuItems} user />
     </div>
-    <div className='Students'> 
+    <div className='Students'>
+      <h6>Students</h6>
+      <h2>4,209</h2>
+      <h6 className='percentages'>62%</h6>
+      <h6 className='percentages'>13%</h6>
+      <h6 className='percentages'>23%</h6>
+
+      <img className='Pie'src={studentImage} alt="Student" />
+    </div> 
     </div>
     </div>
   )
