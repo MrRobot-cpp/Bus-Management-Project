e566f117b762d2c5e0300c0e6f8f1023e798100a
Ahmed Ghazouly <medoghazouly10@gmail.com>
2024-05-04 22:19:50 +0300

    ignore

--- Bus-Management-Project/client/src/Login-Sign/src/components/Driver/TripsTable.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/components/Driver/TripsTable.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import styles from "./TripsTable.module.css";
 
--- Bus-Management-Project/client/src/Login-Sign/src/components/Student/CashDeposit/CashDeposit.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/components/Student/CashDeposit/CashDeposit.jsx
@@ -1,16 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import styles from './CashDeposit.module.css'
+//import Dropdown from '../../General/Dropdown'
 function CashDeposit() {
+    // const options = ['Visa/Credit Card'];
 return (
     <>
     <div  className={styles["cash-deposit-container"]}>
-        
-
-
-
-
+    <div  className={styles["cash-deposit"]}>
+    <div className={styles["cash-deposit-content"]}>
+        <div className="choosebank">
+            
+        </div>
+        </div>
     </div>
+    </div>      
     </>
 )
 }
