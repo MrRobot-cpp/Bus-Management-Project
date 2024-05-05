4757a6e8e79ed6be3c8cff3432e6786a183ebaff
rahmawael0 <rahma2201758miuegypt.edu.eg>
2024-05-05 16:28:27 +0300

    new combo boxes and the alignment of the other 3 boxes

--- Bus-Management-Project/client/src/Login-Sign/src/components/Admin/AdminDashboard.jsx
+++ Bus-Management-Project/client/src/Login-Sign/src/components/Admin/AdminDashboard.jsx
@@ -8,6 +8,11 @@ import iconImage from './Rectangle 2373.png';
 import iconImage1 from './Rectangle 2374.png';
 import iconImage2 from './Rectangle 2375.png';
 import chartimage from './Order.png';
+
+
+
+
+
 function AdminDashboard() {
   const menuItems = [
     {text: "Routes" },
@@ -26,6 +31,7 @@ function AdminDashboard() {
         </div>
 
         {/* da b2a m4 el tany da el awlany w rkz m3aya kda  */}
+        <div className='stylee'>
         <div className='Students'>
   <h6>Students</h6>
     <h2>4,209</h2>
@@ -43,7 +49,16 @@ function AdminDashboard() {
   </div>
 </div>
 
+<div className='combo-row'>
+    <select>
+      <option value="timeframe">TimeFrame: All time</option>
+    </select>
+    </div>
+
+
+</div>
 ِ{/* m4 el box el awlany da el tany  */}
+<div className='stylee'>
 <div className='Students'>
   <h6>Trips</h6>
     <h2>1,302</h2>
@@ -59,6 +74,67 @@ function AdminDashboard() {
     <img className='Pie' src={chartimage} alt="Student" />
   </div>
 </div>
+
+<div className='combo-row'>
+    <select>
+      <option value="source">Source: All</option>
+    </select>
+    </div>
+</div>
+{/* dool b2a ely homa kda ely homa el 3 components  */}
+<div className='test'>
+  <div className='row1'>
+    <div className='compo'>
+      <h6> Recievables</h6>
+      <h2>$25K</h2>
+    </div>
+    <div className='compo'>
+      <h6> Cash Receipts</h6>
+      <h2>$3,500</h2>
+    </div>
+    <div className='compo'>
+      <h6> Expenses Left</h6>
+      <h2>$21.5K</h2>
+    </div>
+  </div>
+
+  <div className='row2'>
+    <div className='compo3'>
+      <h4>Payments and Trips Summary</h4>
+    </div>
+  </div>
+
+  <div className='row3'>
+    <div className='compo1'>
+      <h6>In Progress Trips</h6>
+      <h2>500</h2>
+    </div>
+    <div className='compo1'>
+      <h6> Completed Trips</h6>
+      <h2>150</h2>
+    </div>
+    <div className='compo1'>
+      <h6> Owned Buses</h6>
+      <h2>100</h2>
+    </div>
+  </div>
+
+  
+ {/* 34an lma negy n3ml el combo button wla m4 3rfa esmo eh  */}
+<div className='combo-row1'>
+
+    <select>
+      <option value="medium">Medium: All</option>
+    </select>
+  </div>
+  <div className='compo5'>
+</div>
+
+</div>
+  
+
+
+
 
       </div>
     </div>
