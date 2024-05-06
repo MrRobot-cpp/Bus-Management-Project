e3d6855aaafcbe1b5d469b21848f567c37ba614d
Shady-Yasser <shadyyasset@gmail.com>
2024-05-06 17:46:22 +0300

    ignore

--- Bus-Management-Project/client/src/App.jsx
+++ Bus-Management-Project/client/src/App.jsx
@@ -17,13 +17,8 @@ import React from 'react';
 import Student from './components/Student/Student';
 // import Driver from './components/Driver/Driver'
 //import Home from './components/Home'
-<<<<<<< HEAD
-import Admin from './components/Admin/Admin'
+//import Admin from './components/Admin/Admin'
 //import LandingPage from './components/LandingPage'
-=======
-// import Admin from './components/Admin/Admin'
-// import LandingPage from './components/LandingPage'
->>>>>>> 4889c38db0d19f75f4469f2896726d99a9ca9ad5
 function App() {
     // eslint-disable-next-line no-unused-vars
     const toggleDropDown = () => {
