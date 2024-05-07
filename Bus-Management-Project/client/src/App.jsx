99e2f2c6794cb463ac75254d0964c951cd3fb5e8
Ahmed-Elghazouly <medoghazouly10@gmail.com>
2024-05-08 01:06:05 +0300

    ignore

--- Bus-Management-Project/client/src/App.jsx
+++ Bus-Management-Project/client/src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './components/Login-SignUp/Login.jsx';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from './components/Login-SignUp/Login.jsx';
 //import Signup from './components/signUp.jsx';
 //import StudentProfile from './components/StudentProfile/StudentProfile/';
 //import Driver from './components/Driver/Driver';
@@ -19,14 +19,12 @@ import Student from './components/Student/Student';
 //import Home from './components/Home'
 //import AccountConfig from './components/Login-SignUp/AccountConfig';
 // import Admin from './components/Admin/Admin'
-import Signup from './components/Login-SignUp/signUp';
+// import Signup from './components/Login-SignUp/signUp';
 //import AccountConfig from './components/Login-SignUp/AccountConfig';
 //import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
-import Student from './components/Student/Student';
-import Driver from './components/Driver/Driver'
+// import Driver from './components/Driver/Driver'
 //import Home from './components/Home'
-
-import LandingPage from './components/LandingPage/LandingPage'
+// import LandingPage from './components/LandingPage/LandingPage'
 function App() {
     // eslint-disable-next-line no-unused-vars
     const toggleDropDown = () => {
@@ -39,7 +37,7 @@ function App() {
             {/* <AccountConfig /> */}
             {/* <VerticalNavbar /> */}
            {/*  <Home/> */}
-            {/* <Student/>    */}
+            <Student/>   
             {/* <Driver/> */}
             {/* <Admin /> */}
             {/* <LandingPage/> */}
