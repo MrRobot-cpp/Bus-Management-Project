59441ca2a029b6db796ba5227e02653363e185be
rahmawael0 <rahma2201758miuegypt.edu.eg>
2024-05-08 15:01:55 +0300

    Top Routes Members was updated

--- Bus-Management-Project/client/src/components/Admin/AdminDashboard.jsx
+++ Bus-Management-Project/client/src/components/Admin/AdminDashboard.jsx
@@ -139,9 +139,9 @@ Rakan Waleed
     </select>
     </div>
     <div className='compo6'>
-    <h6>Top Influencers</h6>
+    <h6>Top Routes Members </h6>
     <h6 className='line-break'></h6>
-    <h5> Quizzes taken <span>139</span></h5>
+    <h5> Downtown Cairo to Giza <span>139</span></h5>
     <h5> Questions Answered <span>283</span></h5>
     <h5> Assignments completed <span>782</span></h5>
     <h5> Comments posted <span>1,923</span></h5>
