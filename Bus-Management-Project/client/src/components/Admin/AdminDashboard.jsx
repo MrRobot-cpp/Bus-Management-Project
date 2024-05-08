1911fabeb1e8b17beab66769a0cbd618dd43d55c
rahmawael0 <rahma2201758miuegypt.edu.eg>
2024-05-08 15:27:23 +0300

    Top Routes Members was updated again

--- Bus-Management-Project/client/src/components/Admin/AdminDashboard.jsx
+++ Bus-Management-Project/client/src/components/Admin/AdminDashboard.jsx
@@ -146,7 +146,7 @@ Rakan Waleed
     <h5> Mohandiseen to Maadi <span>283</span></h5>
     <h5> Nasr City to New Cairo <span>782</span></h5>
     <h5> Heliopolis to Old Cairo <span>103</span></h5>
-    <h5> Badges earned  <span>477</span></h5>
+    <h5> Nasr City to CFC <span>477</span></h5>
     <h5> Reinforcements taken <span>280</span></h5>
     </div>
     
