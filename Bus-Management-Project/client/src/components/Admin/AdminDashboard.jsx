9aa8acd283c9732385a9c2d04b45a49ff74e84c1
rahmawael0 <rahma2201758miuegypt.edu.eg>
2024-05-08 16:07:25 +0300

    Top Routes  was updated again

--- Bus-Management-Project/client/src/components/Admin/AdminDashboard.jsx
+++ Bus-Management-Project/client/src/components/Admin/AdminDashboard.jsx
@@ -141,11 +141,11 @@ Rakan Waleed
     <div className='compo6'>
     <h6>Top Routes Members </h6>
     <h6 className='line-break'></h6>
-    <h5> Nozhat to MIU (UNI)   <span>1,500</span></h5>
-    <h5> Rehab to GUC (UNI) <span>1,080</span></h5>
-    <h5> Mohandiseen to AUC (UNI)  <span>383</span></h5>
-    <h5> Mokkataam to MTI (UNI) <span>582</span></h5>
-    <h5> 6th October to MSA (UNI) <span>1000</span></h5>
+    <h5> Nozhat to MIU (UNI)<span>1,500</span></h5>
+    <h5> Rehab to GUC (UNI)<span>1,080</span></h5>
+    <h5> Mohandiseen to AUC (UNI)<span>383</span></h5>
+    <h5> Mokkataam to MTI (UNI)<span>582</span></h5>
+    <h5> 6th October to MSA (UNI)<span>1000</span></h5>
     <h5> New Cairo to  FUE (UNI)<span>680</span></h5>
     <h5> Abbassiya to Cairo (UNI)<span>350 </span></h5>
     </div>
