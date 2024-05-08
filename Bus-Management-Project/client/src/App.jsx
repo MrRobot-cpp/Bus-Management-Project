780273d2499390d45fae9cc234d7e00b7b18f571
Ahmed Samer <ahmedsamersayed22@gmail.com>
2024-05-08 07:59:55 +0300

    TAKE CARE

--- Bus-Management-Project/client/src/App.jsx
+++ Bus-Management-Project/client/src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Login from './components/Login-SignUp/Login.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+ import Login from './components/Login-SignUp/Login.jsx';
 //import Student from './components/Student/Student';
 // import Admin from './components/Admin/Admin'
 // import Signup from './components/Login-SignUp/signUp';
@@ -9,7 +9,6 @@ import React from 'react';
 //import VerticalNavbar from './components/VerticalNavbar/VerticalNavbar';
 // import Student from './components/Student/Student';
 //import Driver from './components/Driver/Driver'
-<<<<<<< HEAD
 //import AccountConfig from './components/Login-SignUp/AccountConfig';
 // import Admin from './components/Admin/Admin'
 import Signup from './components/Login-SignUp/signUp';
@@ -19,9 +18,6 @@ import Student from './components/Student/Student';
 import Driver from './components/Driver/Driver'
 //import Home from './components/Home'
 
-=======
-//import Home from './components/Home'
->>>>>>> eacbaec91e5b6e00412601f921e3833779dfebdb
 import LandingPage from './components/LandingPage/LandingPage'
 function App() {
     // eslint-disable-next-line no-unused-vars
@@ -30,15 +26,7 @@ function App() {
     };
     return (
         <>
-           {/*  <Login /> */}
-            {/* <Signup /> */}
-            {/* <AccountConfig /> */}
-            {/* <VerticalNavbar /> */}
-           {/*  <Home/> */}
-            {/* <Student/>    */}
-            {/* <Driver/> */}
-            {/* <Admin /> */}
-<<<<<<< HEAD
+     {/* SAMER: MA7DSH YL3B FL ROUTES ELY 3AOZ YGRB 7AGA Y COMMENT EL ROUTES W YKTB T7TEHA */}
             <Routes>
             <Route 
               path={"/Landing-page"}
@@ -70,9 +58,7 @@ function App() {
               />
               
             </Routes>
-=======
-            <LandingPage/>
->>>>>>> eacbaec91e5b6e00412601f921e3833779dfebdb
+            {/*SHAYFK YALY BTFKR TSHELHOM HA3DOK */}
         </>
     );
 }
